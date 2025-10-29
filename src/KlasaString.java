66d3c3cb516b0f621dcd4848718a2e3d13bd3785
Josip <ujdur.jo@gmail.com>
2025-10-29 20:31:36 +0100

    Zadatak 3.

---
+++ src/KlasaString.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class KlasaString {
+    public static void main(String[] args) {
+/*
+        String proba = "Danas je srijeda, na predavanju sam! ";
+
+        System.out.println("Veličina: " + proba.length());
+        System.out.println("Znak na indexu: " + proba.charAt(0));
+        System.out.println("Znak na indexu: " + proba.charAt(1));
+        System.out.println("Gdje se nalazi znak: " + proba.indexOf('n'));
+        System.out.println("Ends with: " + proba.endsWith("eda"));
+        System.out.println("Starts with: " + proba.endsWith("sri"));*/
+
+/*        String rijec = "Perica";
+        String rijec1 = new String ("Perica");
+        System.out.println(rijec == rijec1);
+        System.out.print(rijec.equals(rijec1)); // ako radimo sa više stringova ova metoda je bolja*/
+
+       // Zadatak 3: Unijeti u program broj ocjena po želji, ispisati prosjek ocjena. (int brojOcjena = Integer.parseInt(sc.nextLine());)
+        //1. korak -> tražiti od korisnika koliko ocjena želi unijeti ✔
+        //2. korak -> kreirati polje na temelju unesenog broja ✔
+        //3. korak -> pomoću petlje for proći kroz očekivani broj unosa ocjena ✔
+        //4. korak -> pri svakoj vrtnji petlje for tražiti od korisnika da unese ocjenu po ocjenu
+        //svaku ocjenu speramamo u određeni index polja
+        //prilikom čega zbrajamo ocjenu po ocjenu u zasebnu varijablu
+        //5. korak -> računamo i iispisujemo prosjek
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Koliko ocjena želite unijeti?");
+
+        int brojOcjena = Integer.parseInt(sc.nextLine());
+        int zbroj = 0;
+
+        for (int i = 0; i < brojOcjena; i++) {
+            System.out.println("Unesite ocjenu " + (i + 1));
+            int ocjena = Integer.parseInt(sc.nextLine());
+            zbroj += ocjena;
+        }
+
+        double  prosjek = zbroj / brojOcjena;
+        System.out.println("Prosjek ocjena je: " + prosjek);
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,48 @@
+/*
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+*/
+/*
+
 
 public class Main {
     public static void main(String[] args) {
 
+        int[] brojevi = new int[5];
+        brojevi[0] = 1; //naziv polja[indeks] = vrijednost   - indeksi kreću od nule
+        brojevi[1] = 44;
+        brojevi[2] = 9;
+        brojevi[3] = 32;
+        brojevi[4] = 125;
+*//*
 
 
-            System.out.println("i = " + i);
+*/
+/*        // System.out.println(brojevi[1]); // naziv polja + index
+        for (int i = 0; i < brojevi.length; i++)
+            System.out.println(brojevi[i]);
+
+
+        Arrays.sort(brojevi);
+        Arrays.fill(brojevi, 3);
+        for (int i : brojevi) {
+            System.out.println(i);
+        }*//*
+
+
+// ispisati elemente polja unatrag korištenjem petlje for
+
+*/
+/*
+//        for (int i = brojevi.length - 1; i >= 0; i--) {
+
+          for ( Collections.reverse(brojevi);
+            System.out.println(brojevi);
+
+
         }
     }
 }
+*/
+
